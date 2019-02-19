80bdc5503a22c739924fd54339c4605449d49615
noangel <iambin@qq.com>
2019-02-19 12:55:42 +0800

    test

---
+++ xiangyan.js
@@ -0,0 +1,81 @@
+
+//文章列表右则上 u3642436
+function list_ad1(){
+	document.write('<script type="text/javascript" src="//s1.wan1979.com/common/api/89270f.js?gbtaxqx=ta"></script>');
+}
+
+//文章列表右则下 u3642438
+function list_ad2(){
+	document.write('<script type="text/javascript" src="//s1.wan1979.com/source/resource/25hmwt.js?idvc=zszvf"></script>');
+}
+
+//内容页标题下 u3525649
+function show_ad1(){
+	document.write('<script type="text/javascript" src="//s1.wan1979.com/site/production/md8k.js?m=hzewegdp"></script>');
+}
+
+//pc-香烟-内容页-翻页工具上 u3525600
+function show_ad2(){
+	document.write('<script type="text/javascript" src="//s1.wan1979.com/common/static/site/jcxi.js?yt=lqiqszz"></script>');
+}
+
+//pc-香烟-内容页-翻页工具下 u3525603
+function show_ad3(){
+	document.write('<script type="text/javascript" src="//s1.wan1979.com/source/res/3ajc8.js?cxpu=muwdp"></script>');
+}
+
+//pc-香烟-内容页-右侧上 u3642436
+function show_ad4(){
+	document.write('<script type="text/javascript" src="//s1.wan1979.com/common/api/89270f.js?gbtaxqx=ta"></script>');
+}
+
+//pc-香烟-内容页-右侧下 u3642438
+function show_ad5(){
+	document.write('<script type="text/javascript" src="//s1.wan1979.com/source/resource/25hmwt.js?idvc=zszvf"></script>');
+}
+//香烟-pc-品牌内容页-图片上 u3642457
+function show_adp1(){
+	document.write('<script type="text/javascript" src="//s1.wan1979.com/site/zv3wr.js?jewd=atabf"></script>');
+}
+
+//香烟-pc-品牌内容页-评分下 u3525609
+function show_adp2(){
+	document.write('<script type="text/javascript" src="//s1.wan1979.com/source/production/bg3f.js?idva=sacjl"></script>');
+}
+
+//pc-电子烟内容页标题下 u3525649
+function show_addj1(){
+	document.write('<script type="text/javascript" src="//s1.wan1979.com/site/production/md8k.js?m=hzewegdp"></script>');
+}
+
+//pc-电子烟-内容页-翻页工具上
+function show_addj2(){
+	document.write('<script type="text/javascript" src="//s1.wan1979.com/common/static/site/jcxi.js?yt=lqiqszz"></script>');
+}
+
+//pc-电子烟-内容页-翻页工具下
+function show_addj3(){
+	document.write('<script type="text/javascript" src="//s1.wan1979.com/source/res/3ajc8.js?cxpu=muwdp"></script>');
+}
+
+//pc-电子烟-内容页-右侧上
+function show_addj4(){
+	document.write('<script type="text/javascript" src="//s1.wan1979.com/common/api/89270f.js?gbtaxqx=ta"></script>');
+}
+
+//pc-电子烟-内容页-右侧下
+function show_addj5(){
+	document.write('<script type="text/javascript" src="//s1.wan1979.com/source/resource/25hmwt.js?idvc=zszvf"></script>');	
+}
+
+//7、M-香烟-翻页下
+function show_adt(){
+	document.write('');
+}
+
+// pc 内容详情页增加 烟草通微信公众号图片
+function show_addyct(){
+	document.write('<img src="/statics/v2/images/yct.jpg">');
+	
+	
+}
